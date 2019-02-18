9b79c6498e2c75a93c56df42bc582648d524477e
Huong <admin@Apples-MacBook-Pro.net.fpt>
2019-02-18 21:28:25 +0700

    init 2

--- index.php
+++ index.php
@@ -1,7 +1,41 @@
 <?php
+// echo 'Chao ban';
+// echo 'Chao ban';
+// echo 'Chao ban';
+// echo 'Chao ban';
+// echo 'Chao ban';
 
-echo "Hello World!";
 
-echo "Hello World!";
+// 
+// 123456
+
+// echo "Hello World!";
+
+// echo 'Hello World!';
+
+#123456
+
+/**
+ * 345
+ * 1234
+ */
+
+//  345 
+
+//
+// 
+
+
+$a = 12;
+$b = 12.5;
+$s = 'chao ban';
+
+echo gettype($s);
+
+// echo $a;
+
+
+
+
 
 ?>
