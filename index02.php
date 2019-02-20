465b0d5f3b8b9939fff3bd3de9ae1dedf7d12142
Huong <admin@Apples-MacBook-Pro.local>
2019-02-20 20:14:52 +0700

    index02

---
+++ index02.php
@@ -0,0 +1,35 @@
+<?php
+
+// define('pi',3.14);
+// define('pi',3.15);
+
+// $a = 3;
+// $a++; ++$a;
+// echo ++$a; //4
+// echo $a++; //3
+// echo $a;//4
+// $a++;
+
+// echo $a--;
+// echo --$a; // $a = $a-1
+
+
+// $a = 10;
+// $b = 6;
+// $a -= 2; // $a = $a - 2 
+// $a += $b; // $a = $a - $b
+// $a %= $b;   // $a = $a % $b
+// echo $a; //4
+
+$a = 'Chao ban';
+$name = 'Ti';
+// $a .= ' '.$name;  // $a = $a . $name;
+// echo $a .' '. $name;
+$a .= ' $name';  
+// $a .= " $name";  
+
+echo $a;
+
+
+
+?>
