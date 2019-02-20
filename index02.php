042dcef8ca5c157e017d12c185d8dfdd10c5eee0
Huong <admin@Apples-MacBook-Pro.local>
2019-02-20 20:36:25 +0700

    if else

--- index02.php
+++ index02.php
@@ -31,13 +31,49 @@
 // echo $a;
 
 
-$a = 2;
-// $a = $a*$a;
-// $a = pow($a,4);
-$a = $a**4;
+// $a = 2;
+// // $a = $a*$a;
+// // $a = pow($a,4);
+// $a = $a**4;
 
-echo $a;
+// echo $a;
 
 
+// $a = 2; //number
+// $b = '2'; //string
+// if($a != $b){
+//     echo 'True';
+// }
+// else{
+//     echo 'False';
+// }
+// // false
 
+// if($a !== $b){
+//     echo 'True';
+// }
+// else{
+//     echo 'False';
+// }
+//true
+
+$a = 2; //number
+$b = '3'; //string
+
+if($a !== $b){
+    echo 'True';
+}
+else{
+    echo 'False';
+}
+
+// if($a == $b){
+//     echo "$a == $b";
+// }
+// else if($a>$b){
+//     echo "$a > $b";
+// }
+// else{
+//     echo "$a < $b";
+// }
 ?>
