fe6e2cfd81297a49e615316844a60cd44b324f2c
Huong <admin@Apples-MacBook-Pro.local>
2019-02-22 20:22:17 +0700

    array

---
+++ array/index.php
@@ -0,0 +1,27 @@
+<?php
+
+// $arr = [1, 2, 3, 4];
+// $arr2 = array('Mot', 'Hai', 'Ba');
+// $arr3 = [
+//     1, 
+//     'Teo', 
+//     true, 
+//     array(3,5,7)
+// ];
+$arr4 = [
+    'key1'=>2,
+    'key2'=>[
+        7 , 8, 'Hello'
+    ],
+    2 => 12
+];
+// print_r($arr);
+print_r($arr4['key2']);
+// echo $arr4['key2'];
+
+
+
+// echo $arr; // use for string
+
+
+?>
