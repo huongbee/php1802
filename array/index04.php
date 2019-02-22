ca748dd793129e4db8462bdae2ee50b1fae0d4bf
Huong <admin@Apples-MacBook-Pro.local>
2019-02-22 21:27:21 +0700

    foreach

--- array/index04.php
+++ array/index04.php
@@ -10,5 +10,26 @@ $arr4 = array(
     ),
     2 => 12
 );
-print_r($arr4);
+$new = $arr4['key2'];
+// print_r($new);
+foreach($new as $khoa=>$giatri){
+    echo $khoa. ' : '.$giatri;
+    echo "<br>";
+}
+/**
+ * v1 = khoa : 'key_1',  giatri: 7 
+ * v2 = khoa : 0,  giatri: 8
+ * v3 
+ * ... 
+ * v5 = khoa : 5,  giatri: Xin chao
+ */
 
+// print_r($arr4);
+
+
+
+$arr = array('Nguyen', 'Van', 'Ngoc');
+foreach($arr as $value){
+    echo $value;
+    echo "<br>";
+}
