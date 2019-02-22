c9cb32c40476be05a93f4817fbb7f3468c273561
Huong <admin@Apples-MacBook-Pro.local>
2019-02-22 21:15:20 +0700

    index04

---
+++ array/index04.php
@@ -0,0 +1,14 @@
+<?php
+$arr4 = array(
+    'key1'=>2,
+    'key2'=> array(
+        'key_1' =>  7,
+                    8, 
+        'key_3' =>  'Hello',
+            4   =>  'Xin chao',
+                   'Xin chao'
+    ),
+    2 => 12
+);
+print_r($arr4);
+
