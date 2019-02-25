13932a687107bef6503e7849419c631df4fe1838
Huong <admin@Apples-MacBook-Pro.local>
2019-02-25 21:14:37 +0700

    data

---
+++ array/baitap/lib/data.php
@@ -0,0 +1,53 @@
+<?php
+
+$link = 'https://cdn.tgdd.vn/Products/Images/42/';
+$arrProduct = [
+    [
+        "name"=>'iPhone Xs Max 64GB',
+        'image'=>'190321/iphone-xs-max-gray-400x460.png',
+        'price'=>'33900000'
+    ],
+    [
+        "name"=>'Samsung Galaxy A9 (2018)',
+        'image'=>'192003/samsung-galaxy-a9-2018-blue-400x460.png',
+        'price'=>'12900000'
+    ],
+    [
+        "name"=>'iPhone 7 32GB',
+        'image'=>'74110/iphone-7-32gb-den-400x460.png',
+        'price'=>'12300000'
+    ],
+    [
+        "name"=>'Samsung Galaxy Note 8',
+        'image'=>'106211/samsung-galaxy-note8-black-400x460.png',
+        'price'=>'14900000'
+    ],
+    [
+        "name"=>'iPhone Xs Max 64GB',
+        'image'=>'iphone-xs-max-gray-400x460.png',
+        'price'=>'33900000'
+    ],
+    [
+        "name"=>'Huawei Mate 20',
+        'image'=>'179736/huawei-mate-20-black-400x460.png',
+        'price'=>'8900000'
+    ],
+    [
+        "name"=>'OPPO R17 Pro',
+        'image'=>'186395/oppo-r17-pro-2-400x460.png',
+        'price'=>'16900000'
+    ],
+    [
+        "name"=>'iPhone Xs Max 64GB',
+        'image'=>'190321/iphone-xs-max-gray-400x460.png',
+        'price'=>'33900000'
+    ],
+    [
+        "name"=>'iPhone Xs Max 64GB',
+        'image'=>'190321/iphone-xs-max-gray-400x460.png',
+        'price'=>'33900000'
+    ],
+]
+
+
+?>
