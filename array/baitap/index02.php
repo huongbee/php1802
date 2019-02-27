2d558da71c8125f47d4c305d3d414a6209a382c2
Huong <admin@Apples-MacBook-Pro.local>
2019-02-27 21:06:02 +0700

    show product

--- array/baitap/index02.php
+++ array/baitap/index02.php
@@ -11,66 +11,35 @@
 
 <body>
     <div class="container">
+        <?php
+            require_once 'lib/data.php';
+        ?>
+
+        <?php foreach($arrProduct as $p): ?>
         <div class="sanpham">
             <div class="hinhsp">
                 <img src="images/iphone-xs-max-gray-400x460.png">
             </div>
             <div class="thongtin">
-                <h2>Samsung Galaxy S8</h2>
+                <h2><?php echo $p['name']?></h2>
                 <p>
                     Giảm ngay 2 triệu (không áp dụng đồng thời Thu cũ đổi mới)
                 </p>
                 <p>Đổi máy cũ lên đời A9 tiết kiệm đến 12.000.000đ (áp dụng từ 1/2 - 28/2)</p>
-                <p>Khuyến mãi</p>
+                <p class="khuyenmai">Khuyến mãi</p>
                 <p>Cơ hội trúng 100 bao lì xì trị giá 2 tỷ đồng Xem chi tiết</p>
             </div>
             <div class="tensanpham">
-                <p>Samsung Galaxy S8</p>
+                <p><?=$p['name']?></p>
             </div>
             <div class="dongia">
-                <p>8.900.000 vnd</p>
-            </div>
-        </div>
-        <div class="sanpham">
-            <div class="hinhsp">
-                <img src="images/iphone-xs-max-gray-400x460.png">
-            </div>
-            <div class="thongtin">
-                <h2>Samsung Galaxy S8</h2>
-                <p>
-                    Giảm ngay 2 triệu (không áp dụng đồng thời Thu cũ đổi mới)
-                </p>
-                <p>Đổi máy cũ lên đời A9 tiết kiệm đến 12.000.000đ (áp dụng từ 1/2 - 28/2)</p>
-                <p>Khuyến mãi</p>
-                <p>Cơ hội trúng 100 bao lì xì trị giá 2 tỷ đồng Xem chi tiết</p>
-            </div>
-            <div class="tensanpham">
-                <p>Samsung Galaxy S8</p>
+                <p><?= number_format($p['price'])?> VND</p>
             </div>
             <div class="dongia">
-                <p>8.900.000 vnd</p>
+                <p><?= number_format($p['price'], 0, ',', '.')?> VND</p>
             </div>
         </div>
-        <div class="sanpham">
-            <div class="hinhsp">
-                <img src="images/iphone-xs-max-gray-400x460.png">
-            </div>
-            <div class="thongtin">
-                <h2>Samsung Galaxy S8</h2>
-                <p>
-                    Giảm ngay 2 triệu (không áp dụng đồng thời Thu cũ đổi mới)
-                </p>
-                <p>Đổi máy cũ lên đời A9 tiết kiệm đến 12.000.000đ (áp dụng từ 1/2 - 28/2)</p>
-                <p>Khuyến mãi</p>
-                <p>Cơ hội trúng 100 bao lì xì trị giá 2 tỷ đồng Xem chi tiết</p>
-            </div>
-            <div class="tensanpham">
-                <p>Samsung Galaxy S8</p>
-            </div>
-            <div class="dongia">
-                <p>8.900.000 vnd</p>
-            </div>
-        </div>
+        <?php endforeach?>
     </div>
 </body>
 
