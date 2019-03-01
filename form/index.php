04ef0c289e212a70128efb90b46d51e5d70af6e3
Huong <admin@Apples-MacBook-Pro.local>
2019-03-01 20:23:53 +0700

    form 2

--- form/index.php
+++ form/index.php
@@ -10,7 +10,7 @@
     <form>
         Username: <input type="text" name="txtUsername" placeholder="Username" value="Admin">
         <br><br>
-        Birthdate: <input type="date" name="txtBirthdate" placeholder="Birthdate" value="2019/12/2">
+        Birthdate: <input type="date" name="txtBirthdate" placeholder="Birthdate" value="2019-12-20">
         <br><br>
         Password: <input type="password" name="txtPw" placeholder="Pw">
         <br><br>
@@ -21,10 +21,29 @@
             <input type="radio" name="gender" value="female" id="txtFemale"> 
             <label for="txtFemale">Female</label>
         <br><br>
+        Message: <textarea name="txtMessage" rows="3">123456</textarea>
+        <br><br>
+        Course: 
+        <select name="txtCourse">
+            <option value="PHP">PHP & MySQL</option>
+            <option value="Mean">Mean</option>
+            <option value="React">React</option>
+            <option value="Android">Android</option>
+        </select>
+        <br><br>
+        Course: 
+        <select name="txtCourse" multiple>
+            <option value="PHP">PHP & MySQL</option>
+            <option value="Mean">Mean</option>
+            <option value="React">React</option>
+            <option value="Android">Android</option>
+        </select>
+        <br><br>
         <button type="reset">Reset</button>
         <button type="button">No action</button>
         <button type="submit">Send</button>
     </form>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 </body>
 </html>
 <!-- 
@@ -37,6 +56,4 @@
     datetime
     radio
     checkbox
-
-
  -->
