94906be367304581228336d37467a767ee9ba5ae
Huong <admin@Apples-MacBook-Pro.local>
2019-03-01 20:12:22 +0700

    form

---
+++ form/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        Username: <input type="text" name="txtUsername" placeholder="Username" value="Admin">
+        <br><br>
+        Birthdate: <input type="date" name="txtBirthdate" placeholder="Birthdate" value="2019/12/2">
+        <br><br>
+        Password: <input type="password" name="txtPw" placeholder="Pw">
+        <br><br>
+        Gender: 
+            <label>
+                <input type="radio" name="gender" value="male"> Male
+            </label>
+            <input type="radio" name="gender" value="female" id="txtFemale"> 
+            <label for="txtFemale">Female</label>
+        <br><br>
+        <button type="reset">Reset</button>
+        <button type="button">No action</button>
+        <button type="submit">Send</button>
+    </form>
+</body>
+</html>
+<!-- 
+    text
+    url
+    range
+    color
+    pw
+    date
+    datetime
+    radio
+    checkbox
+
+
+ -->
