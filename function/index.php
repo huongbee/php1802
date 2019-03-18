b591434a95456180a036463097769bbe9db9f0de
Huong <huongnguyenak96@gmail.com>
2019-03-18 21:36:18 +0700

    function

---
+++ function/index.php
@@ -0,0 +1,26 @@
+<?php
+
+function getName($name){
+    return $name;
+}
+function showData($data){
+    return "Hello, ". $data('Nguyen Van A');
+}
+// echo showData('getName');
+
+$a = 'b';
+$b = 1;
+
+echo $$a;
+
+
+
+
+
+
+
+
+
+
+
+?>
