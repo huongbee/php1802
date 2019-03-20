273cbd7f3967eb1238291a6b682a504b325a5997
Huong <huongnguyenak96@gmail.com>
2019-03-20 20:15:53 +0700

    $

--- function/index03.php
+++ function/index03.php
@@ -19,10 +19,38 @@
 // }
 // echo sum([3,5,4,5,7,6,7,3]);
 
-function sum(int ...$arr):float{
-    return array_sum($arr);
-}
-echo sum(3,5,4,5,7,6,7,3,6,5.5);
+// function sum(int ...$arr):float{
+//     return array_sum($arr);
+// }
+// echo sum(3,5,4,5,7,6,7,3,6,5.5);
 
+// function sum($count,int ...$arr):float{
+//     return $count = count($arr);
+//     // return array_sum($arr);
+// }
+// $count = 0;
+// $count = sum($count,5,4,5,7,6,7,3,6);
+// echo $count;
+
+// function sum($count,int ...$arr):array{
+//     $count = count($arr);
+//     $tong = array_sum($arr);
+//     return [
+//         'count'=>$count,
+//         'tong'=>$tong
+//     ];
+// }
+// $count = 0;
+// $data = sum($count,5,4,5,7,6,7,3,6);
+// print_r($data);
+
+function sum(&$count,int ...$arr):float{
+    $count = count($arr);
+    $tong = array_sum($arr);
+    return $tong;
+}
+$dem = 0;
+$tong = sum($dem,5,4,5,7,6,7,3,6);
+echo $dem;
 
 ?>
