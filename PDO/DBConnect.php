6de1b70feac6ea32ff3e2f749d276ea17cd40e95
Huong <huongnguyenak96@gmail.com>
2019-04-03 20:38:11 +0700

    executeQuery

--- PDO/DBConnect.php
+++ PDO/DBConnect.php
@@ -1,17 +1,36 @@
 <?php
+ 
 class DBConnect{
     private $options = [];
-
+    private $connect = null;
+    /**
+     * Connect to database
+     */
     function __construct($dbName='php1802_shopping',$username='root', $password=''){
         try{
-            $connect = new PDO("mysql:dbname=$dbName;host=127.0.0.1",$username,$password,$this->options);
-            $connect->exec('set names utf8');
+            $this->connect = new PDO("mysql:dbname=$dbName;host=127.0.0.1",$username,$password,$this->options);
+            $this->connect->exec('set names utf8');
         }
         catch(PDOException $e){
             echo $e->getMessage();
             die('Connect fail!');
         }
     }
+
+    /**
+     * Use for INSERT | UPDATE | DELETE
+     * @param string $sql
+     * @param array $values = []
+     * @return bool
+     */
+    function executeQuery(string $sql, array $values=[]){
+        $stmt = $this->connect->prepare($sql);
+        for($i=1; $i<=count($values); $i++){
+            $stmt->bindValue($i,$values[$i-1]);
+        }
+        return $stmt->execute();
+    }
+
 }
 
 ?>
--- PDO/test.php
+++ PDO/test.php
@@ -3,8 +3,15 @@
 require_once 'DBConnect.php';
 
 $con = new DBConnect();
+// $sql = "INSERT INTO users(username,email,password) 
+//         VALUES('admin1234','admin1234@gmail.com','123456')";
 
-// $sql = 'UPDATE ....';
+$sql = "UPDATE users
+        SET username = 'admin1234-update'
+        WHERE id=23";
+$check = $con->executeQuery($sql);
 
+if($check) echo 'Updated!';
+else echo 'Error';
 
 ?>
